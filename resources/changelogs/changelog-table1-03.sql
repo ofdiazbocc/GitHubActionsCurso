0c848c7e47ee3643bebbe4c3319e132ccb59be34
ofdiaz <47216530+OmarFabianDiaz@users.noreply.github.com>
2021-01-04 09:46:45 -0500

    scripts simultaneos

--- resources/changelogs/changelog-table1-03.sql
+++ resources/changelogs/changelog-table1-03.sql
@@ -1,5 +1,5 @@
 --liquibase formatted sql
 
 --changeset Omar:01_insert_test1
-insert into test1 (id, description) values (11, 'Description for 11';
+insert into test1 (id, description) values (11, 'Description for 11');
 --rollback delete from test1 where id in (11);
